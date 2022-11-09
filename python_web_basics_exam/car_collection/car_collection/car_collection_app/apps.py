435a16fc97c3ce709bfd14c0b1b71f473be04aa7
Elena Ilieva <elena.98mi@gmail.com>
2022-11-09 18:51:44 +0200

    Added 'Car collection' Django project.

--- python_web_basics_exam/car_collection/car_collection/car_collection_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CarCollectionAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'car_collection.car_collection_app'
